700520dd429fec22dd2fb06dccb50ab985dc5d70
mcginav <74389679+mcginav@users.noreply.github.com>
2021-04-22 17:58:49 -0700

    pgAdmin Table created
    Adding the query to create the table. Had some errors at the last line of original code. Dropped "FOREIGN KEY (primary_key) REFERENCES winemag-data_first150k(_c0)
    to avoid the error, but may need to fix that if that information is needed.
    Added a png of the table headers that were created.

---
+++ clean.sql
@@ -0,0 +1,22 @@
+CREATE TABLE cleaned_data (
+   primary_key INT NOT NULL,
+   points FLOAT,
+   price FLOAT,
+   country INT,
+   designation INT,
+   province INT,
+   region_1 INT,
+   variety INT,
+   winery INT,
+   "1" FLOAT,
+   "2" FLOAT,
+   "3" FLOAT,
+   "4" FLOAT,
+   "5" FLOAT,
+   "6" FLOAT,
+   "7" FLOAT,
+   "8" FLOAT,
+   "9" FLOAT,
+   "10" FLOAT,
+   PRIMARY KEY (primary_key)
+);
